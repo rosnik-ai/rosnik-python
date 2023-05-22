27adbd745323fc3bc74ab51dc691f9fffd0533b5
Nick DiRienzo <me@nickdirienzo.com>
2023-05-22 00:59:09 -0700

    auto init maybe

--- promptly/__init__.py
+++ promptly/__init__.py
@@ -1,3 +1,13 @@
+import os
+
+from . import env
 from .client import init
 
 __all__ = ["init"]
+
+def auto_init():
+    api_key = env.get_api_key()
+    if api_key is not None:
+        init()
+
+auto_init()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,8 @@
+import threading
+
 def test_init(mocker):
     mocker.patch("promptly.platforms.openai._patch_openai")
     mocker.patch("threading.Thread")
     import promptly
     promptly.init()
+    threading.Thread.assert_called_once()
