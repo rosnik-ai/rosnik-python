268dd24d85ed3e37b6bcb02d75d492cc7ba72140
Nick DiRienzo <me@nickdirienzo.com>
2023-10-08 03:16:28 -0700

    format

--- rosnik/journey.py
+++ rosnik/journey.py
@@ -1,14 +1,14 @@
-
 from contextvars import ContextVar
 from typing import Optional
 
 from ulid import monotonic as ulid
 
 # TODO: this should work for flask apps. need to check.
-journey_id_cv: ContextVar[Optional[str]] = ContextVar('journey_id')
+journey_id_cv: ContextVar[Optional[str]] = ContextVar("journey_id")
 
 # TODO: turn into a config or smth
-JOURNEY_TIMEOUT = 30 * 60 # 30 minutes
+JOURNEY_TIMEOUT = 30 * 60  # 30 minutes
+
 
 def get_or_create_journey_id():
     _new_id = ulid.new()
@@ -19,12 +19,12 @@ def get_or_create_journey_id():
     if existing_journey_id is None:
         journey_id_cv.set(new_id_str)
         return new_id_str
-    
+
     # If we have one, and it's stale, then make a new one.
     existing_timestamp = existing_journey_id.timestamp().timestamp
     if existing_journey_id and now_from_new_id - existing_timestamp > JOURNEY_TIMEOUT:
         journey_id_cv.set(new_id_str)
         return new_id_str
-    
+
     # Otherwise the one we have works.
     return existing_journey_id
