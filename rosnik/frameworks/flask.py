70b208b300365d72324ea231e6be29499d934077
Nick DiRienzo <me@nickdirienzo.com>
2023-10-10 11:46:18 -0700

    flask integration

---
+++ rosnik/frameworks/flask.py
@@ -0,0 +1,29 @@
+import logging
+from rosnik import headers, interaction, journey
+
+logger = logging.getLogger(__name__)
+
+
+def _get_journey_id():
+    try:
+        from flask import request
+    except ImportError:
+        logger.warning("flask is not installed")    
+    journey_id = request.headers.get(headers.JOURNEY_ID_KEY)
+    interaction_id = request.headers.get(headers.INTERACTION_ID_KEY)
+    journey.set_journey_id(journey_id)
+    interaction.set_interaction_id(interaction_id)
+
+def _annotate_response_headers(response):
+    response.headers[headers.JOURNEY_ID_KEY] = journey.get_journey_id()
+    response.headers[headers.INTERACTION_ID_KEY] = interaction.get_interaction_id()
+    return response
+
+class FlaskRosnik:
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.before_request(_get_journey_id)
+        app.after_request(_annotate_response_headers)
--- rosnik/journey.py
+++ rosnik/journey.py
@@ -1,22 +1,27 @@
+import logging
 from contextvars import ContextVar
 from typing import Optional
 
 from ulid import monotonic as ulid
+
+logger = logging.getLogger(__name__)
 
 journey_id_cv: ContextVar[Optional[str]] = ContextVar("journey_id")
 
 # TODO: turn into a config or smth
 JOURNEY_TIMEOUT = 30 * 60  # 30 minutes
 
+def get_journey_id():
+    return journey_id_cv.get(None)
 
 def get_or_create_journey_id():
     _new_id = ulid.new()
     new_id_str = _new_id.str
     now_from_new_id = int(_new_id.timestamp().timestamp)
-    existing_journey_id = journey_id_cv.get(None)
+    existing_journey_id = get_journey_id()
     # If we don't have one, make one.
     if existing_journey_id is None:
-        journey_id_cv.set(new_id_str)
+        set_journey_id(new_id_str)
         # TODO: this needs to be sent to the frontend
         return new_id_str
 
@@ -24,10 +29,17 @@ def get_or_create_journey_id():
     existing_journey_ulid = ulid.parse(existing_journey_id)
     existing_timestamp = existing_journey_ulid.timestamp().timestamp
     if existing_journey_id and now_from_new_id - existing_timestamp > JOURNEY_TIMEOUT:
-        journey_id_cv.set(new_id_str)
+        set_journey_id(new_id_str)
         # TODO: this needs to be sent to the frontend
         return new_id_str
 
     # Otherwise the one we have works.
     # TODO: this needs to be sent to the frontend.
     return existing_journey_id
+
+
+def set_journey_id(journey_id: str):
+    existing_journey_id = get_journey_id()
+    if existing_journey_id:
+        logger.warning(f"Overwriting journey ID from {existing_journey_id} to {journey_id}")
+    journey_id_cv.set(journey_id)
