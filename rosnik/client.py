733ad34e6b474f075ac1de74afd625b26b84f3fa
Nick DiRienzo <me@nickdirienzo.com>
2023-10-15 00:35:45 -0700

    client tests

--- rosnik/client.py
+++ rosnik/client.py
@@ -11,13 +11,15 @@ try:
     openai_enabled = True
     from rosnik.providers import openai as openai_
 except ImportError:
-    logger.debug("Skipping OpenAI instrumentation.")
+    pass
 
 
 def init():
     if openai_enabled:
         logger.debug("OpenAI is enabled. Patching.")
         openai_._patch_openai()
+    else:
+        logger.debug("Skipping OpenAI instrumentation.")
 
     if env.is_sync():
         logger.debug("Running in sync mode")
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,33 @@
-from rosnik.events import queue
-
+import rosnik
 
 def test_init(mocker):
-    mocker.patch("rosnik.providers.openai._patch_openai")
-    mocker.patch("rosnik.events.queue.start_event_processor")
-    import rosnik
-
+    mock_patch = mocker.patch("rosnik.providers.openai._patch_openai")
+    mock_logger = mocker.patch('rosnik.client.logger.debug')
+    
     rosnik.init()
-    queue.start_event_processor.assert_called_once()
+    mock_patch.assert_called_once()
+    mock_logger.assert_called_once_with("OpenAI is enabled. Patching.")
+
+def test_openai_not_imported(mocker):
+    mocker.patch('rosnik.client.openai_enabled', False)
+    mock_logger = mocker.patch('rosnik.client.logger.debug')
+    
+    rosnik.init()
+    
+    mock_logger.assert_called_with("Skipping OpenAI instrumentation.")
+
+def test_sync_mode(mocker):
+    mocker.patch('rosnik.env.is_sync', return_value=True)
+    mock_logger = mocker.patch('rosnik.client.logger.debug')
+    
+    rosnik.init()
+    
+    mock_logger.assert_called_with("Running in sync mode")
+
+def test_not_sync_mode(mocker):
+    mocker.patch('rosnik.env.is_sync', return_value=False)
+    mock_logger = mocker.patch('rosnik.client.logger.debug')
+    
+    rosnik.init()
+    
+    mock_logger.assert_called_once_with("OpenAI is enabled. Patching.")
