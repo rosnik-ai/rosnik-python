277fa6856ae6bbaeb9b1d669e088c28660ce6922
Nick DiRienzo <me@nickdirienzo.com>
2023-10-16 15:46:13 -0700

    bump to 0.0.26

--- tests/test_ai_events.py
+++ tests/test_ai_events.py
@@ -18,7 +18,7 @@ def validate_common_attributes(event: Event):
     assert event._metadata.environment is None
     assert event._metadata.runtime == platform.python_implementation()
     assert event._metadata.runtime_version == platform.python_version()
-    assert event._metadata.sdk_version == "0.0.25"
+    assert event._metadata.sdk_version == "0.0.26"
     assert event._metadata.function_fingerprint
     assert len(event._metadata.function_fingerprint.split(".")) == 5
 
