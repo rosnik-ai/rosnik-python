91735689fa42a4feb620ca751cc2845258cd3e7b
Nick DiRienzo <me@nickdirienzo.com>
2023-10-16 13:32:47 -0700

    move init to init_app

--- rosnik/frameworks/flask_rosnik.py
+++ rosnik/frameworks/flask_rosnik.py
@@ -35,11 +35,13 @@ def _annotate_response_headers(response):
 
 class FlaskRosnik:
     def __init__(self, app=None, api_key=None, sync_mode=None, environment=None):
+        self.api_key = api_key
+        self.sync_mode = sync_mode
+        self.environment = environment
         if app is not None:
             self.init_app(app)
-
-        client.init(api_key=api_key, sync_mode=sync_mode, environment=environment)
 
     def init_app(self, app):
         app.before_request(_before_request)
         app.after_request(_annotate_response_headers)
+        client.init(api_key=self.api_key, sync_mode=self.sync_mode, environment=self.environment)
